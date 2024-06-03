46cc8b7a3542141eda6b939a1d017890b9c8e5c0
MeghanaMN18 <meghanannayakkrn@gmail.com>
2024-06-03 16:00:27 +0530

    h file

---
+++ catt.h
@@ -0,0 +1,12 @@
+#ifndef FCATERING_H_INCLUDED
+#define FCATERING_H_INCLUDED
+
+#include <stdio.h>
+void displayMenu();
+void calculateCost(int num_people, int cost_per_person, const char *event_name, const char *serving_style);
+void marriage();
+void namingCeremony();
+void birthdayParty();
+void houseWarming();
+
+
