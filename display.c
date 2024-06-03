f2e80d05d74625f6dfaf0b306e153a9e9a62364b
MeghanaMN18 <meghanannayakkrn@gmail.com>
2024-06-03 15:02:48 +0530

    display

---
+++ display.c
@@ -0,0 +1,10 @@
+#include <fcatering>
+void displayMenu() {
+    printf("\n===== Catering Service Menu =====\n");
+    printf("1. Marriage Function\n");
+    printf("2. Naming Ceremony\n");
+    printf("3. Birthday Party\n");
+    printf("4. House Warming Ceremony\n");
+    printf("5. Exit\n");
+    printf("=================================\n");
+}
