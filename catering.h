f50cad1a0ffacdd6bfc337525a97c034e9ba689a
Meghana M.N <138748001+MeghanaMN18@users.noreply.github.com>
2024-06-04 10:59:45 +0530

    Update catering.h

--- catering.h
+++ catering.h
@@ -1,7 +1,10 @@
 #ifndef CATERING_H_INCLUDED
 #define CATERING_H_INCLUDED
 
+
 #include <stdio.h>
+#include <string.h>
+
 void displayMenu();
 void calculateCost(int num_people, int cost_per_person, const char *event_name, const char *serving_style);
 void marriage();
