73f6b291bfc6a61fb77ff5798e83889a6e076406
Meghana M.N <138748001+MeghanaMN18@users.noreply.github.com>
2024-07-28 23:15:33 +0530

    Update catering.h

--- catering.h
+++ catering.h
@@ -1,26 +1,33 @@
+
 #ifndef CATERING_H_INCLUDED
 #define CATERING_H_INCLUDED
-<<<<<<< HEAD
-
-#include <stdio.h>
-#include  <string.h>
-=======
 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
->>>>>>> f0e3f666cdefc242d9d83f4f40a2a4290ff9638d
 
-void displayMenu();
-void calculateCost(int num_people, int cost_per_person, const char *event_name, const char *serving_style);
+#define MAX_ITEMS 20
+
+void clearInputBuffer();
+int getValidInt();
+void displayWelcome();
+void displayUserMenu();
+void displayServingStyles();
+int getServingStyleCost(char *serving_style);
+void calculateCost(int num_people, int base_cost_per_person, int serving_style_cost, const char *event_name, const char *serving_style);
+void displayItems(const char items[MAX_ITEMS][30], int count);
+void addItem(char items[MAX_ITEMS][30], int *count, const char new_item[30]);
+void deleteItem(char items[MAX_ITEMS][30], int *count);
+void modifyItems(char items[MAX_ITEMS][30], int *count);
+void saveOrderToFile(const char *event_name, int num_people, int base_cost_per_person, int serving_style_cost, char items[MAX_ITEMS][30], int item_count, const char *serving_style);
+void viewTransactions();
 void marriage();
 void namingCeremony();
 void birthdayParty();
-void houseWarming();
+void houseWarmingCeremony();
 
 #endif
-<<<<<<< HEAD
-=======
 
->>>>>>> f0e3f666cdefc242d9d83f4f40a2a4290ff9638d
 
