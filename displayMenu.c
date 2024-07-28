291e22f6f4f03d3d04ee01e3ba89cf5e4c94fa89
Meghana M.N <138748001+MeghanaMN18@users.noreply.github.com>
2024-07-28 23:27:39 +0530

    Update displayMenu.c

--- displayMenu.c
+++ displayMenu.c
@@ -1,10 +1,10 @@
 #include "catering.h"
 void displayUserMenu() {
     printf("\n===== Catering Service Menu =====\n");
-    printf("1. Marriage Function \n");
-    printf("2. Naming Ceremony \n");
-    printf("3. Birthday Party \n");
-    printf("4. House Warming Ceremony \n");
+    printf("1. Marriage_Function \n");
+    printf("2. Naming_Ceremony \n");
+    printf("3. Birthday_Party \n");
+    printf("4. House_Warming_Ceremony \n");
     printf("5. Exit\n");
     printf("=================================\n");
 }
