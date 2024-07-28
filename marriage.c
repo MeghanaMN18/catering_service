f1338fc1b30fc3726bbdaaf6dc153fb1438a7b2e
Meghana M.N <138748001+MeghanaMN18@users.noreply.github.com>
2024-07-28 22:04:30 +0530

    Update marriage.c

--- marriage.c
+++ marriage.c
@@ -1,37 +1,33 @@
 #include "catering.h"
 void marriage() {
+    char items[MAX_ITEMS][30] = {
+        "Vegetable Biryani", "Paneer Butter Masala", "Mixed Veg Curry", "Naan", "Palak Paneer",
+        "Gulab Jamun", "Rasmalai", "Kaju Katli", "Jalebi"
+    };
+    int item_count = 9;
+
     printf("\n===== Marriage Catering Service Menu =====\n");
-    printf("1. Vegetable Biryani\n");
-    printf("2. Paneer Butter Masala\n");
-    printf("3. Mixed Veg Curry\n");
-    printf("4. Naan\n");
-    printf("5. Palak Paneer\n");
-    printf("6. Gulab Jamun\n");
-    printf("7. Rasmalai\n");
+    displayItems(items, item_count);
     printf("===========================================\n");
 
-    int serving_choice;
-    printf("Enter the preferred serving style (1 for buffet, 2 for plated, 3 for family-style): ");
-    scanf("%d", &serving_choice);
+    modifyItems(items, &item_count);
 
-    char serving_style[20];
-    if (serving_choice == 1) {
-        strcpy(serving_style, "Buffet");
-    } else if (serving_choice == 2) {
-        strcpy(serving_style, "Plated");
-    } else if (serving_choice == 3) {
-        strcpy(serving_style, "Family-style");
-    } else {
-        printf("Invalid choice! Defaulting to Buffet.\n");
-        strcpy(serving_style, "Buffet");
-    }
+    char serving_style[30];
+    int serving_style_cost = getServingStyleCost(serving_style);
 
     int num_people;
-    printf("Enter the number of people attending the marriage: ");
-    scanf("%d", &num_people);
+    do {
+        printf("Enter the number of people attending the marriage (min 50 people): ");
+        num_people = getValidInt();
+        if (num_people < 50) {
+            printf("Please enter a valid number of people.\n");
+        }
+    } while (num_people < 50);
 
-    calculateCost(num_people, 450, "Marriage", serving_style); // Assuming each person costs Rs. 450 for marriage
+    calculateCost(num_people, 450, serving_style_cost, "Marriage", serving_style);
 
-    printf("Preferred service style: %s\n", serving_style);
+    displayItems(items, item_count);
     printf("Thank you for choosing our service!\n");
+
+    saveOrderToFile("Marriage", num_people, 450, serving_style_cost, items, item_count, serving_style);
 }
