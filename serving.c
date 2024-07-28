5d676dc9414b67bd921c16b2b8016610dc8bc055
MeghanaMN18 <meghanannayakkrn@gmail.com>
2024-07-28 22:43:11 +0530

    commit

---
+++ serving.c
@@ -0,0 +1,9 @@
+#include "catering.h"
+
+void displayServingStyles() {
+    printf("\n===== Serving Styles =====\n");
+    printf("1. Buffet (Rs. 50 per person)\n");
+    printf("2. Plated (Rs. 100 per person)\n");
+    printf("3. Family-Style (Rs. 75 per person)\n");
+    printf("===========================\n");
+}
