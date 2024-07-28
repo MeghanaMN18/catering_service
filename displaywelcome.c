bd422ea2623fab9c6684d2f59334094b131cf8de
MeghanaMN18 <meghanannayakkrn@gmail.com>
2024-07-28 22:41:30 +0530

    commit

---
+++ displaywelcome.c
@@ -0,0 +1,10 @@
+#include "catering.h"
+
+void displayWelcome() {
+    printf("\n========================================\n");
+    printf("   Welcome to our Catering Service App  \n");
+    printf("========================================\n");
+    printf("We are delighted to serve you the best\n");
+    printf("catering services for your special events.\n");
+    printf("========================================\n");
+}
