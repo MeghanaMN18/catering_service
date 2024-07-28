cd26be39533b0b0cb80bec36d5527b4a0cd15e28
Meghana M.N <138748001+MeghanaMN18@users.noreply.github.com>
2024-07-28 23:14:39 +0530

    Update displaywelcome.c

--- displaywelcome.c
+++ displaywelcome.c
@@ -2,7 +2,7 @@
 
 void displayWelcome() {
     printf("\n========================================\n");
-    printf("   Welcome to our Catering Service App  \n");
+    printf("   Welcome to ABC Catering Service App  \n");
     printf("========================================\n");
     printf("We are delighted to serve you the best\n");
     printf("catering services for your special events.\n");
